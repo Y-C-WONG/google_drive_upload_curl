96e12941a44a18f34a87c06a3cb0aacb5b694a78
Y-C-WONG <141507805+Y-C-WONG@users.noreply.github.com>
2023-10-22 22:59:28 +0100

    Update upload_to_drive.sh

--- scripts/upload_to_drive.sh
+++ scripts/upload_to_drive.sh
@@ -1,1 +1,50 @@
+#!/bin/bash
+
+# This script upload file to google drive using google drive API
+# Author: Yau Chuen Wobng in October, 2023
+# -----------------------------------------
+# For first time useing google drive api, please get the authorization code, 
+# then exchange refresh token with the authorization code by content the google authorization api.
+# The authorization code only need once in most of the time.
+# The refresh token, once generated, it will be expired if not been used in six months of time.
+# The refresh token uses for exchange the access token which generate by the google authorization api.
+# Then access token need to be generated everytime before upload file.
+# -----------------------------------------
+# STEP 1: Get the authorization Code
+# Visit below link  with web browser by provide the client id and scope, then Authorization will return in the URL link 
+# https://accounts.google.com/o/oauth2/auth?client_id=[client_id]&redirect_uri=http://localhost&response_type=code&scope=[scope]&access_type=offline
+# -----------------------------------------
+# STEP 2: Get the refresh token
+# run below command to exchange the refresh token with the authorization code, client id, and client secret
+# curl --request POST --data "code=[AUTH_CODE]&client_id=[CLIENT_ID]&client_secret=[CLIENT_SECRET]v&redirect_uri=http://localhost&grant_type=authorization_code" https://oauth2.googleapis.com/token
+# this api call will return a json response that contains the refresh token
+# ------------------------------------------
+# STEP 3: Get the access token
+# Fill in below information and run the _getAccessToken()
+CLIENT_ID=""
+SCOPE=""
+AUTH_CODE=""
+CLIENT_SECRET=""
+REFRESH_TOKEN=""
+function _getAccessToken() 
+{
+    ACCESS_TOKEN=$(curl --request POST -s \
+    --data "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&refresh_token=$REFRESH_TOKEN&grant_type=refresh_token" \
+    https://oauth2.googleapis.com/token | jq ."access_token")
+}
+
+UPLOAD_FILE=""
+MIMETYPE="Application/zip"
+DRIVE_FOLDER_ID=""
+
+function _uploadToGoogleDrive()
+{
+    RESPONSE_JSON=$(curl -X POST -L -H "Authorization: Bearer $ACCESS_TOKEN" \
+    -F "metadata={name : '$UPLOAD_FILE', parents : ['$DRIVE_FOLDER_ID']};type=application/json;charset=UTF-8" \
+    -F "file=@$UPLOAD_FILE;type=$MIMETYPE" "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart")
+
+    ERROR_CODE=$(echo $RESPONSE_JSON | jq .error.code)
+    DRIVE_FILE_NAME=$(echo $RESPONSE_JSON | jq .name)
+    DRIVE_FILE_ID=$(echo $RESPONSE_JSON | jq .id)
+}
 
