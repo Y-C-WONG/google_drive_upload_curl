7ba23ac959c942b632e7f186e99299ee63ee34b2
Y-C-WONG <141507805+Y-C-WONG@users.noreply.github.com>
2023-11-08 20:43:33 +0000

    Update drive_config.sh

--- scripts/drive_config.sh
+++ scripts/drive_config.sh
@@ -11,4 +11,4 @@ DRIVE_FOLDER_ID=""
 AUTH_REQ_URL="https://accounts.google.com/o/oauth2/auth?client_id=$CLIENT_ID&redirect_uri=http://localhost&response_type=code&scope=https://www.googleapis.com/auth/drive&access_type=offline"
 REFRESH_REQ_DATA="code=$AUTH_CODE&client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&grant_type=authorization_code&redirect_uri=http://localhost"
 TOKEN_REQ_API_URL="https://oauth2.googleapis.com/token"
-KEEP_No_FILE=3
+KEEP_No_FILES=3
